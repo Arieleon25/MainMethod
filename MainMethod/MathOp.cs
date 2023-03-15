4575106e790fdba9c2ce5f1f791edb79502329ba
Arieleon25 <Arieleon25@yahoo.com>
2023-03-15 19:46:57 -0400

    Add project files.

---
+++ MainMethod/MathOp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MathOp
+    {
+        public int Sum(int i, int i2)
+        {
+            int num1 = i;
+            int num2 = i2;
+            return num1 + num2;
+        }
+
+        public decimal DecSum(decimal i, int i2)
+        {
+            decimal num1 = i;
+            int num2 = i2;
+            return num1 - num2;
+        }
+
+        public int StrSum(string i, int i2)
+        {
+            string str1 = i;
+            int parse = Int32.Parse(str1);
+            int num1 = i2 * parse;
+            return num1;
+        }
+
+    }
+}
+
---
+++ MainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp Operaciones = new MathOp();
+            int input = 5;
+            int input2 = 6;
+            int results = Operaciones.Sum(input, input2);
+
+            Console.WriteLine(results);
+
+            decimal input3 = 12.013547m;
+            int input4 = 6;
+            decimal results2 = Operaciones.DecSum(input3, input4);
+
+            Console.WriteLine(results2);
+
+            string input5 = "50";
+            int input6 = 5;
+            int results3 = Operaciones.StrSum(input5, input6);
+
+            Console.WriteLine(results3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
